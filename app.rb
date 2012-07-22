64a27fe8363cf5e8f531ed3a646abd55fe30d5d9
Sean Porter <portertech@gmail.com>
2012-07-22 15:04:15 -0700

    attempt to get the latest issue when running on heroku

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require "sinatra"
+require File.join(File.dirname(__FILE__), "lib", "issue")
 
 get "/" do
   "Hello world!"
+end
+
+get "/latest" do
+  content_type "application/json"
+  latest = Acetone::Issue.new.latest.reject {|key, value| key == "_id" }
+  latest.to_json
 end
