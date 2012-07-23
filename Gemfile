35ae419aa4c70935743c4c51834b8977b6f82fdc
Sean Porter <portertech@gmail.com>
2012-07-23 15:55:39 -0700

    created a web ui, ready to serve

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source :rubygems
 gem "mongo"
 gem "bson_ext"
 gem "instapaper"
+gem "haml"
 gem "sinatra"
--- app.rb
+++ app.rb
@@ -1,55 +1,62 @@
 require "sinatra"
+require "haml"
 require "json"
 require File.join(File.dirname(__FILE__), "lib", "issue")
 require File.join(File.dirname(__FILE__), "lib", "user")
 require File.join(File.dirname(__FILE__), "lib", "instapaper")
 
+enable :sessions
+
 get "/" do
-  "Hello world!"
+  haml :index, :locals => {
+    :success => session.delete(:success),
+    :error   => session.delete(:error)
+  }
 end
 
 get "/latest" do
   content_type "application/json"
   issue = Acetone::Issue.new
   issue.latest!
-  body JSON.generate(issue.to_hash)
+  JSON.generate(issue.to_hash)
 end
 
 post "/user" do
   request.body.rewind
-  data = JSON.parse(request.body.read)
-  valid = %w[username password latest_issue].all? do |key|
-    data.has_key?(key)
+  valid   = true
+  success = false
+  error   = false
+  unless params.has_key?("username") && !params[:username].empty?
+    valid = false
   end
   if valid
     instapaper = Acetone::Instapaper.new
-    if instapaper.get_access_token(data["username"], data["password"])
+    if instapaper.get_access_token(params[:username], params[:password])
       if credentials = instapaper.current_credentials
         user = Acetone::User.new
         user.user_id            = credentials[:user_id]
         user.oauth_token        = credentials[:oauth_token]
         user.oauth_token_secret = credentials[:oauth_token_secret]
-        unless data["latest_issue"]
+        unless params[:latest_issue]
           issue = Acetone::Issue.new
           issue.latest!
           user.last_issue = issue.created
         end
         if user.save!
-          body ""
+          success = "Success! You are now subscribed!"
         else
-          status 500
-          body ""
+          error = "Error! Please try again later."
         end
       else
-        status 401
-        body ""
+        error = "Error! Invalid Instapaper credentials."
       end
     else
-      status 401
-      body ""
+      error = "Error! Invalid Instapaper credentials."
     end
   else
-    status 400
-    body ""
+    error = "Error! You must provide a valid Instapaper Username."
   end
+  session[:success] = success
+  session[:error]   = error
+  redirect back
 end
